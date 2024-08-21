5eabf2f8b301d861ae48b23548d9816dbdb4057a
Kyle <kylepanurignan@gmail.com>
2024-08-21 14:56:06 +0800

    Update

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import { Box, Typography, Button, IconButton, styled } from "@mui/material";
 import EmailIcon from "@mui/icons-material/Email";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import Resume from "../../assets/resume/Kyle_Panuringan_Resume.pdf";
+import Resume from "../../assets/Kyle_Panuringan_Resume.pdf";
 import hero from "../../assets/hero.jpg";
 import heroMobile from "../../assets/heroMobile.jpg";
 
